a3c33d21b47ca2d8e3ff73164df61f5c6eb58608
EnesPINAR <enespinar2@icloud.com>
2022-08-18 22:29:25 +0300

    counter animation

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,19 +1,25 @@
+gsap.registerPlugin(ScrollTrigger);
 const counts = document.querySelectorAll(".count");
 const interval = 2500;
-
 counts.forEach((count) => {
-    let startVal = 0;
-    let endVal = parseInt(count.getAttribute("count"));
-    let duration = Math.floor(interval / endVal);
+    gsap.to(count, {
+        scrollTrigger: count,
+        rotation: 0,
+        duration: 0.1,
+        onComplete: () => {
+            let startVal = 0;
+            let endVal = parseInt(count.getAttribute("count"));
+            let duration = Math.floor(interval / endVal);
 
-    let counter = setInterval(
-        () => {
-            startVal += 1;
-            count.textContent = startVal;
-            if (startVal == endVal) {
-                clearInterval(counter);
-            }
-        }, duration
-    )
-
-});
+            let counter = setInterval(
+                () => {
+                    startVal += 1;
+                    count.textContent = startVal;
+                    if (startVal == endVal) {
+                        clearInterval(counter);
+                    }
+                }, duration
+            )
+        }
+    })
+})
